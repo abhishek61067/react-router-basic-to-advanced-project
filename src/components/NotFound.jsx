764785d74997bb8a687ceee2acbb60f2845a13cd
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-04-09 09:19:39 +0545

    chore: work with not found page

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not found</h1>
+      <Link to="/">Go back to homepage</Link>
+    </div>
+  );
+};
+
+export default NotFound;
